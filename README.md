7082bfb5da8da2ce1dda682b20dcfec7e61b149e
KrzysztofMularski <krzysztof.mularski@student.put.poznan.pl>
2023-01-09 02:38:49 +0100

    xata + wstępny interfejs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## System Do Automatycznego Pomiaru Prędkości Ejekcji i Injekcji Płyty CD Z Natychmiastowym Pomiarem Szybkości Odczytu Baselinowej Zawartości Płyty
 
-## Setup:
+Setup:
 
 ```
 git clone https://github.com/KrzysztofMularski/SyDAPoPEjIP-tyCZNaPomSO-BaZaP
@@ -13,6 +13,8 @@ npm run setup
 
 npm run dev
 ```
+
+Jeszcze trzeba ręcznie skopiować plik `.env` znajdujący się w `resources/js/` i zamienić go na `.env-local` i wypełnić go kluczem z bazy Xata.
 
 Aby generować automatycznie plik css po zmianach (najlepiej odpalić sobie w osobnym terminalu):
 
--- resources/js/main.js
+++ resources/js/main.js
@@ -2,9 +2,74 @@ function onWindowClose() {
     Neutralino.app.exit();
 }
 
+const envs = {};
+
+const setEnvVars = async () => {
+    const content = await Neutralino.filesystem.readFile(
+        "./resources/js/.env-local"
+    );
+    content.split("\r\n").forEach((s) => {
+        const [key, value] = s.split("=");
+        envs[key] = value;
+    });
+};
+
+const getAllRecords = async () => {
+    const pomiary = document.getElementById("pomiary");
+    const body = {
+        page: {
+            size: 15,
+        },
+    };
+    const options = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${envs.XATA_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    };
+
+    try {
+        let response = await fetch(`${envs.DB_URL}/query`, options);
+        response = await response.json();
+        let arr = response.records;
+
+        arr = arr.map(
+            ({ measured_time, date }, id) =>
+                `<p>${id}. ${measured_time} (${date})</p>`
+        );
+        pomiary.innerHTML = arr.join(" ");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const insertRecord = async (time) => {
+    const body = {
+        measured_time: time,
+        date: new Date(),
+    };
+    const options = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${envs.XATA_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    };
+    try {
+        await fetch(`${envs.DB_URL}/data?columns=id`, options);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 let pomiarId;
 
 async function enterPomiarsLoop() {
+    insertRecord(1222);
+    return;
     const liczba_iteracji = 1;
     for (let i = 0; i < liczba_iteracji; i++) {
         const pomiar = await Neutralino.os.spawnProcess(
@@ -19,6 +84,25 @@ function onPomiar() {
 }
 
 Neutralino.init();
+
+setEnvVars();
+
+(() => {
+    const container = document.getElementById("intro-container");
+    const intro = document.getElementById("intro");
+    const containerWidth = container.offsetWidth;
+    const width = intro.offsetWidth;
+    let offset = 0;
+    setInterval(() => {
+        if (width < containerWidth) {
+            if (offset < -width) {
+                offset = containerWidth;
+            }
+            intro.style = `translate: ${offset}px`;
+            offset -= 16;
+        }
+    }, 400);
+})();
 
 Neutralino.events.on("windowClose", onWindowClose);
 
